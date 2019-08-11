2c7b4cb4ed9c3d15e656e9a0081a454170265986
Wojtek <golewski.wojciech@outlook.com>
2019-08-11 20:46:00 +0200

    deleted unneded component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { MainComponent } from './components/main/main.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, MainComponent, HomeComponent, NotFoundComponent],
+  declarations: [HeaderComponent, FooterComponent, HomeComponent, NotFoundComponent],
   imports: [
     CommonModule
   ],
-  exports: [HeaderComponent, FooterComponent, MainComponent, HomeComponent, NotFoundComponent]
+  exports: [HeaderComponent, FooterComponent, HomeComponent, NotFoundComponent]
 })
 export class SharedModule { }
