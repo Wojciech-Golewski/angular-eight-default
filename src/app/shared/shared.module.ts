eb00e2a06ef86401c78cb62148daa940206a19b9
Wojtek <golewski.wojciech@outlook.com>
2019-08-11 20:33:53 +0200

    home component added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,14 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MainComponent } from './components/main/main.component';
+import { HomeComponent } from './pages/home/home.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, MainComponent],
+  declarations: [HeaderComponent, FooterComponent, MainComponent, HomeComponent],
   imports: [
     CommonModule
   ],
-  exports: [HeaderComponent, FooterComponent, MainComponent]
+  exports: [HeaderComponent, FooterComponent, MainComponent, HomeComponent]
 })
 export class SharedModule { }
