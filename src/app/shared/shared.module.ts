d7b74b81c9e4dc302264c1e4ac851859430cb272
Wojtek <golewski.wojciech@outlook.com>
2019-08-11 20:35:43 +0200

    not found page added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,14 +4,15 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MainComponent } from './components/main/main.component';
 import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, MainComponent, HomeComponent],
+  declarations: [HeaderComponent, FooterComponent, MainComponent, HomeComponent, NotFoundComponent],
   imports: [
     CommonModule
   ],
-  exports: [HeaderComponent, FooterComponent, MainComponent, HomeComponent]
+  exports: [HeaderComponent, FooterComponent, MainComponent, HomeComponent, NotFoundComponent]
 })
 export class SharedModule { }
