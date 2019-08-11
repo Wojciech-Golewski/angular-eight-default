d3eec3151de07f03b22376b87d205f476909c75e
Wojtek <golewski.wojciech@outlook.com>
2019-08-11 21:13:32 +0200

    header replaced by nav component from @angular/material

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { NavComponent } from './components/nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, HomeComponent, NotFoundComponent],
+  declarations: [FooterComponent, HomeComponent, NotFoundComponent, NavComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
-  exports: [HeaderComponent, FooterComponent, HomeComponent, NotFoundComponent]
+  exports: [FooterComponent, HomeComponent, NotFoundComponent, NavComponent]
 })
 export class SharedModule { }
