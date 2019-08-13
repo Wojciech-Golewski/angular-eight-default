9c83ecdd3d223f8f8a6a611715724bf0c2293b46
Wojtek <golewski.wojciech@outlook.com>
2019-08-13 20:49:28 +0200

    added simplest logic for the not-found component be routed automatically to main after short timeout

--- src/app/shared/pages/not-found/not-found.component.ts
+++ src/app/shared/pages/not-found/not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
@@ -7,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotFoundComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) {}
 
   ngOnInit() {
+    setTimeout(() => this.route.navigateByUrl('/'), 3000);
   }
-
 }
