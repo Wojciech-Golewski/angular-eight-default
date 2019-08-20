dc9e8ceba705f32226d8e359285ccef1a93e75bb
Wojtek <golewski.wojciech@outlook.com>
2019-08-20 20:41:05 +0200

    main module generated

--- src/app/core/main/main.module.ts
+++ src/app/core/main/main.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MainModule } from './main/main.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
-    MainModule
+    CommonModule
   ]
 })
-export class CoreModule { }
+export class MainModule { }
