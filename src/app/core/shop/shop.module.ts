eda84f158f92f88678a52dfc16caef16031f7557
Wojtek <golewski.wojciech@outlook.com>
2019-08-20 20:54:18 +0200

    shop-list component generated

--- src/app/core/shop/shop.module.ts
+++ src/app/core/shop/shop.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ShopListComponent } from './components/shop-list/shop-list.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [ShopListComponent],
   imports: [
     CommonModule
   ]
