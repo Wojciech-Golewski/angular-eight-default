ec58f811527117f04bd75b11957cb64e7a282f34
Wojtek <golewski.wojciech@outlook.com>
2019-08-25 14:47:30 +0200

    generated shop-main component

--- src/app/core/shop/shop.module.ts
+++ src/app/core/shop/shop.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopListComponent } from './components/shop-list/shop-list.component';
 import { ShopRoutingModule } from './shop-routing.module';
+import { ShopMainComponent } from './components/shop-main/shop-main.component';
 
 @NgModule({
-  declarations: [ShopListComponent],
+  declarations: [ShopListComponent, ShopMainComponent],
   imports: [
     ShopRoutingModule,
     CommonModule
-  ]
+  ],
+  exports: [ShopMainComponent]
 })
 export class ShopModule { }
