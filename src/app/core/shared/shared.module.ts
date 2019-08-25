f5f6e15ef2863c92f25dc2430446df883f028f35
Wojtek <golewski.wojciech@outlook.com>
2019-08-25 15:08:01 +0200

    generated search component

--- src/app/core/shared/shared.module.ts
+++ src/app/core/shared/shared.module.ts
@@ -7,15 +7,16 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { RouterModule } from '@angular/router';
 import { FilterAndSortComponent } from './components/filter-and-sort/filter-and-sort.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
-  declarations: [FooterComponent, NotFoundComponent, HeaderComponent, FilterAndSortComponent],
+  declarations: [FooterComponent, NotFoundComponent, HeaderComponent, FilterAndSortComponent, SearchComponent],
   imports: [
     CommonModule,
     FontAwesomeModule,
     AngularFontAwesomeModule,
     RouterModule
   ],
-  exports: [FooterComponent, NotFoundComponent, HeaderComponent]
+  exports: [FooterComponent, NotFoundComponent, HeaderComponent, SearchComponent]
 })
 export class SharedModule { }
